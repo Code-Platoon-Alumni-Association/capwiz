ebe04862cf5968e3b897a34e62ce30c0e694c0d7
Scott Plunkett <splunkett@8thLight.com>
2019-10-24 17:17:20 -0500

    Updates

---
+++ precache-manifest.1606edf879913fb626b9a35871d72e97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44efc65b805ee12697be2d4115ea684e",
+    "url": "/capwiz/index.html"
+  },
+  {
+    "revision": "945736682a3494b4fcda",
+    "url": "/capwiz/static/css/main.835e0009.chunk.css"
+  },
+  {
+    "revision": "c74e8830798dfebd8121",
+    "url": "/capwiz/static/js/2.9742864d.chunk.js"
+  },
+  {
+    "revision": "945736682a3494b4fcda",
+    "url": "/capwiz/static/js/main.ddb3ba0e.chunk.js"
+  },
+  {
+    "revision": "5d84c8469414acc05280",
+    "url": "/capwiz/static/js/runtime-main.e47b5a70.js"
+  },
+  {
+    "revision": "f37d995ee97ef03210857238b7fe76f0",
+    "url": "/capwiz/static/media/logo.f37d995e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/capwiz/app/precache-manifest.505487d5181d19b71be358e68a46656b.js"
+  "/capwiz/precache-manifest.1606edf879913fb626b9a35871d72e97.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capwiz/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/capwiz/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
